fee46a9f957d168c75459bae030b236472a47863
NovaEric <ericnova23@hotmail.com>
2022-11-13 16:37:43 -0600

    Finished Part 1

--- Solution.py
+++ Solution.py
@@ -2,22 +2,38 @@ def main():
     number=0
     unlock='232361'
     lock='232364'
-    exitOut=70
+    exitOut='000000'
     print("The system is locked")
     sw=True
     while sw:
         try:
-            number=int(input("Enter the code to unlock or lock the system, to exit press 70 "))
-            if unlock in (str(number)):
-                print("System unlocked")
-            elif lock in (str(number)):
-                print("System locked")
-            elif number==exitOut:
-                sw=False
+            number=input("Enter 6 digits code to unlock or lock the system, to exit press 000000\n")
+            validatedInput = checkInput(number)
+            if validatedInput:
+                if unlock in (str(number)):
+                    print("System unlocked")
+                elif lock in (str(number)):
+                    print("System locked")
+                elif exitOut in (str(number)):
+                    sw=False
         except ValueError:
-            print("Must be an integer and between 0-9, to exit enter 70")
+            print("Must be an integer and between 0-9, to exit enter 000000\n")
 
-      
+
+def checkInput(digits: str):
+    counter = 0
+    if digits.isnumeric():
+        for digit in digits:
+            counter += 1
+        
+        if counter == 6:
+            return True
+        else:
+            return False
+            
+    else:
+        return False
+
   
 main()
 #913520451033444232364
